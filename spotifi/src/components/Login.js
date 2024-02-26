0faf17ee71b4b1f3f5e97d3f42f2e445023bbe2a
SujayVivek <sujayv2016@gmail.com>
2024-02-27 01:32:37 +0530

    Login with Spotify

--- spotifi/src/components/Login.js
+++ spotifi/src/components/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
         src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
         alt="Spotify logo"
       />
-      <a href="#">LOGIN WITH SPOTIFY</a>
+      <a href="#">LOGIN WITH CLOTIFY</a>
     </div>
   )
 }
