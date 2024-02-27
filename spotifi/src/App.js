bc3a59c48fb60fa7ec7fefcf74bd98fd869bdad8
SujayVivek <sujayv2016@gmail.com>
2024-02-28 01:47:26 +0530

    Next up- Install spotify web api

--- spotifi/src/App.js
+++ spotifi/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Login'
+import { getTokenFromURL } from './components/Spotify';
 
 const App = () => {
+  const [token,setToken]= useState(0);
+
+  useEffect(()=>{
+    const hash = getTokenFromURL();
+    window.location.hash="";    //this is done so that the access token is not revealed to the user..access token is seen only to the code now!
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token);
+    }    
+  },[])
   return (
     <>
     <div className='App'>
-      <Login/>
+      {token && <Login/>}
       
     </div>
     </>
